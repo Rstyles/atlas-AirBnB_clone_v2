8f38f3ffa10b1984d2948e7e7680c72de594f194
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-24 15:19:34 -0500

    mocked up more unit tests

---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for testing db storage
+"""
+
+import unittest
+from models.base_model import BaseModel
+
+
+class test_dbstorage(unittest.TestCase):
+    """
+    Test class for testing db storage
+    """
+
+    def test_dbstorage(self):
+        pass
+
+    def setup_mysql_test(self):
+        pass

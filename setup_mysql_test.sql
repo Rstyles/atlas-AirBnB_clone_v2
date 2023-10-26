ce97073b14893317c99212943ac4a3ab3be27b4d
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-26 10:28:28 -0500

    fixed

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,11 +1,11 @@
 -- Creates a test bd
 
-CREATE DATABASE IF NOT EXISTS hbtn_test_db;
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE USER IF NOT EXISTS 'hbtn_user'@'localhost' IDENTIFIED BY 'hbtn_pass';
+CREATE USER IF NOT EXISTS 'hbtn_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbtn_test_db.* TO 'hbtn_user'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbtn_user'@'localhost';
 
-GRANT SELECT ON hbtn_test_db.* TO 'hbtn_user'@'localhost';
+GRANT SELECT ON hbnb_test_db.* TO 'hbtn_user'@'localhost';
 
 FLUSH PRIVILEGES;

705c567bb34b139371376bacda9f5d355e22c00c
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-26 10:57:09 -0500

    formatting and fixes

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,7 @@
 -- Creates a test bd
-
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
-
-CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+REVOKE ALL PRIVILEGES ON *.*
+FROM 'hbnb_test' @'localhost';
+GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test' @'localhost';

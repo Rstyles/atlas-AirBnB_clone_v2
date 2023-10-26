6f55a398d02ccb958a059212aa31c406583a383a
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-26 10:58:14 -0500

    change order

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,7 @@
 -- Creates a test bd
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
 REVOKE ALL PRIVILEGES ON *.*
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
 FROM 'hbnb_test' @'localhost';
 GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test' @'localhost';

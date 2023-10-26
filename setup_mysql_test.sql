b9594e85ebc326b9db6bb2527086b7addf21c0d6
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-26 11:08:08 -0500

    push another

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,7 +1,5 @@
--- Creates a test bd
+-- Create hbnh_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test' @'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-REVOKE ALL PRIVILEGES ON *.*
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
-FROM 'hbnb_test' @'localhost';
-GRANT SELECT ON hbnb_test_db.* TO 'hbnb_test' @'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test' @'localhost';

7c03aa8b165174118a0b0a35f4cbbcdc9fad07cc
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-30 14:26:52 -0500

    fix

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-import os
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage
+import os
 
 
 class State(BaseModel, Base):
@@ -11,17 +13,16 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete, delete-orphan")
+    cities = relationship(
+        "City", backref="state", cascade="all, delete", passive_deletes=True
+    )
 
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
 
         @property
         def cities(self):
-            from models import storage
-            from models.city import City
-
-            list_cities = []
-            for city in storage.all(City).values():
+            city_list = []
+            for city in storage.all(City):
                 if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+                    city_list.append(city)
+            return city_list

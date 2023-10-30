4e8079c6ff4208ff6c6f4408383a3c48d8b4f13b
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-30 14:29:45 -0500

    revert

--- models/state.py
+++ models/state.py
@@ -21,8 +21,8 @@ class State(BaseModel, Base):
 
         @property
         def cities(self):
-            city_list = []
-            for city in storage.all(City):
+            list_cities = []
+            for city in storage.all(City).values():
                 if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+                    list_cities.append(city)
+            return list_cities

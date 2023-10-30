4e85df08e10cd807f3bfba34ab0875ff62c05f82
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-30 10:08:53 -0500

    fixes

--- models/base_model.py
+++ models/base_model.py
@@ -17,9 +17,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        if kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")

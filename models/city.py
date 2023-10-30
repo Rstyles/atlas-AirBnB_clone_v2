1cebfe9958b011688b0ac5897a5a0d2ed46b8dd2
Ryan Styles <ryan.c.styles@gmail.com>
2023-10-30 15:22:49 -0500

    changes

--- models/city.py
+++ models/city.py
@@ -3,10 +3,13 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """ The city class """
+    """The city class"""
+
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete", passive_deletes=True)

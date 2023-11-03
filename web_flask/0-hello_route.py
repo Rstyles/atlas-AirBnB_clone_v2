71d2616bb140a189fc3115614a8de81d97747e3d
Ryan Styles <ryan.c.styles@gmail.com>
2023-11-03 15:56:49 -0500

    added files for 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+# Create the Flask application instance
+app = Flask(__name__)
+
+# Define routes and views
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+# Run the Flask application
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)

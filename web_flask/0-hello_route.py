4328e04f6eb5a6df10c993da46cb3b0ab1dd8602
Ryan Styles <ryan.c.styles@gmail.com>
2023-11-03 16:05:26 -0500

    fixed docstring and formatting

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-from flask import Flask, request
 """Runs a Flask web application"""
-    
+from flask import Flask, request
+
+
 app = Flask(__name__)
 
-# Define routes and views
-@app.route('/')
-def hello():
-    return 'Hello HBNB!'
 
-# Run the Flask application
-if __name__ == '__main__':
+@app.route("/")
+def hello():
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

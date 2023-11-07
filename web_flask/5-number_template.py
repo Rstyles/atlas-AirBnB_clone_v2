11e4d753221be57217697ea9db7e34f27c8cb611
Ryan Styles <ryan.c.styles@gmail.com>
2023-11-07 10:06:00 -0600

    changed h1 to H1 and fixed formatting

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -31,9 +31,11 @@ def python(text):
 def number(n):
     return "{:d} is a number".format(n)
 
+
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', n=n)
+    return render_template("5-number.html", n=n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

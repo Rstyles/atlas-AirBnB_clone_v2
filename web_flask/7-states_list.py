bfe5c70db1f9a0d62a565943082e10ca718c9d60
Ryan Styles <ryan.c.styles@gmail.com>
2023-11-08 13:12:45 -0600

    fix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,6 +4,7 @@ The application listens on 0.0.0.0, port 5000
 Routes:
     /states_list: display an HTML page
 """
+
 from flask import Flask, render_template, request
 from models import storage
 from models.state import State
@@ -11,9 +12,20 @@ from models.state import State
 app = Flask(__name__)
 
 
+from flask import Flask, render_template
+from typing import Dict, List
+
+app = Flask(__name__)
+
+
 @app.route("/states_list", strict_slashes=False)
-def states_list():
-    states = storage.all(State).values
+def states_list() -> str:
+    """
+    Returns a rendered HTML template with a list of all states.
+
+    :return: A string representing the rendered HTML template.
+    """
+    states: List[Dict[str, str]] = storage.all(State).values()
     return render_template("7-states_list.html", states=states)
 
 

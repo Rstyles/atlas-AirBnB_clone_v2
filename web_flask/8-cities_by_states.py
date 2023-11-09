6791c8ce608990a1d68c157231cd5661bbb32bfd
Ryan Styles <ryan.c.styles@gmail.com>
2023-11-09 15:43:57 -0600

    changes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -4,6 +4,7 @@ The application listens on 0.0.0.0, port 5000
 Routes:
     /states_list: display an HTML page
 """
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -14,7 +15,7 @@ app = Flask(__name__)
 
 @app.route("/cities_by_states", strict_slashes=False)
 def cities_by_states():
-    states = storage.all(State)
+    states = storage.all(State).values()
     return render_template("8-cities_by_states.html", states=states)
 
 
